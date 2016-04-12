994b5ddf7995bb3d42e0bb5c340e1373ee538e6f
chenqg <sweetlifetime@163.com>
2016-04-12 18:05:20 +0800

    小蛐蛐  你猜猜猜

---
+++ IanAlertDemo/IanLoadAlert/XIAOQUQU.h
@@ -0,0 +1,17 @@
+//
+//  XIAOQUQU.h
+//  IanLoadAlert
+//
+//  Created by chenqg on 16/4/12.
+//  Copyright © 2016年 ian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XIAOQUQU : NSObject
+
+/** name */
+@property (nonatomic, strong) NSString *name;
+
+
+@end
